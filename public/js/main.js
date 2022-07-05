e58960b432833400dd1d6f3b2763c70f1a9e97a4
hsargeant <hssoftwareengineering@gmail.com>
2022-07-05 17:16:15 -0400

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,131 @@
+let h2s = Array.from(document.querySelectorAll('h2'))
+let storage = Object.keys(localStorage)
+
+h2s.forEach((x,i)=>{
+    if(storage.includes(x.innerText)){
+        h2s[i].parentNode.parentNode.childNodes[5].childNodes[3].style.color = '#333'
+        h2s[i].parentNode.parentNode.childNodes[5].childNodes[3].classList.add('liked')
+        h2s[i].parentNode.parentNode.childNodes[5].childNodes[3].classList.remove('no-like')
+
+    }
+    
+})
+
+const thumbText = document.querySelectorAll('.no-like')
+const deleteText = document.querySelectorAll('.fa-trash')
+const clickedLike = document.querySelectorAll('.liked')
+
+
+Array.from(clickedLike).forEach((element)=>{
+    element.addEventListener('click',removeLike)
+})
+
+Array.from(thumbText).forEach((element)=>{
+    element.addEventListener('click', addLike)
+})
+
+
+Array.from(deleteText).forEach((element)=>{
+    element.addEventListener('click', deleteMovie)
+})
+
+async function addLike(){
+
+    const movieName =this.parentNode.parentNode.childNodes[1].innerText
+    const currentlikes = Number(this.parentNode.childNodes[1].innerText)
+    // store likes
+    localStorage.setItem(movieName,"liked")
+
+    try{
+        const response = await fetch('addOneLike', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'movieName': movieName,
+              'currentLikes': currentlikes
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}
+
+ async function removeLike(){
+    console.log('click to remove')
+
+    const movieName =this.parentNode.parentNode.childNodes[1].innerText
+    const currentlikes = Number(this.parentNode.childNodes[1].innerText)
+
+    if(currentlikes ==0){
+        return
+    }
+    // remove like from storage
+    localStorage.removeItem(movieName)
+
+    try{
+        const response = await fetch('removeLike', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'movieName': movieName,
+              'currentLikes': currentlikes
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}
+
+//delete movie
+
+
+// Filter Movies
+
+let input = document.querySelector('#search')
+input.addEventListener('keyup',filterCards)
+
+function filterCards(){
+  let section=document.querySelectorAll('.card')
+  let txtValue;
+  let filter = input.value.toUpperCase();
+  let h2 = document.querySelectorAll('h2')
+  for (i = 0; i < h2.length; i++) {
+    txtValue=h2[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      section[i].style.display = "";
+    } else {
+        section[i].style.display = "none";
+    }
+  }
+}
+
+let popUp = document.querySelector('.aboutPopup')
+let about = document.querySelector('.aboutMenu')
+
+let addPop = document.querySelector('.addMovie')
+let addLink = document.querySelector('.addLink')
+
+console.log(about,"about")
+about.addEventListener('click',()=>{
+    console.log('click')
+    popUp.classList.remove('hide')
+})
+
+document.querySelector('.closeAbout').addEventListener('click',()=>{
+    popUp.classList.add('hide')
+})
+
+addLink.addEventListener('click',()=>{
+    addPop.classList.remove('hide')
+})
+
+document.querySelector('.closeAdd').addEventListener('click',()=>{
+    addPop.classList.add('hide')
+
+})
---
+++ server.js
@@ -0,0 +1,119 @@
+//express + MONGO SET UP
+const express = require("express")
+const app = express()
+const PORT = 8500
+const MongoClient = require("mongodb").MongoClient
+// const cors = require("cors")
+require('dotenv').config()
+
+let db
+const dbName = process.env.DBNAME
+const connectionString = process.env.MONGO_URL
+
+MongoClient.connect(connectionString,{ useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/',(req, res)=>{
+
+    db.collection('movie-names').find().sort({likes: -1}).toArray()
+    .then(data => {
+        res.render('index.ejs', { movie: data })
+    })
+    .catch(error => console.error(error))
+})
+
+
+
+//add movie to database
+
+app.post('/addMovie', async (req, res) => {
+    if(!req.body.name){
+        res.json('enter a valid movie')
+        res.redirect('/')
+        return
+    }
+    let movieName = req.body.name.toLowerCase()
+    let year = req.body.year
+
+    //get api data
+        const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=0ad3ff819fd0c4926c23cadfaecd9bf7&query=${movieName}&year=${year}`)
+        const data = await response.json()
+        if(data.total_results==0){
+            res.redirect('/')
+            return
+        }
+        const path = `https://image.tmdb.org/t/p/original${data.results[0].poster_path}` || 'https://skydomepictures.com/wp-content/uploads/2018/08/movie-poster-coming-soon-2.png'
+
+
+        //check if 
+        const check = await db.collection('movie-names').find().toArray()
+        if(check.find(elem=>elem.name ==data.results[0].original_title )){
+            res.redirect('/')
+            return
+        }
+    //send to db
+    const post = await db.collection('movie-names').insertOne({name: data.results[0].original_title,
+    image: path, year: data.results[0].release_date.split("-")[0],  likes: 0})
+
+    console.log('Movie Added')
+    res.redirect('/')
+     
+})
+
+//add likes
+app.put('/addOneLike', (req, res) => {
+    let updatelikes = req.body.currentLikes + 1
+    db.collection('movie-names').updateOne({name: req.body.movieName},{
+        $set: {
+            likes: updatelikes
+          }
+    },{
+        sort: {likes: -1},
+        upsert: true
+    })
+    .then(result => {
+        console.log('Added One Like')
+        res.json('Like Added')
+    })
+    .catch(error => console.error(error))
+
+})
+app.put('/removeLike', (req, res) => {
+    let updatelikes =req.body.currentLikes - 1
+    db.collection('movie-names').updateOne({name: req.body.movieName},{
+        $set: {
+            likes: updatelikes
+          }
+    },{
+        sort: {likes: -1},
+        upsert: true
+    })
+    .then(result => {
+        console.log('removed One Like')
+        res.json('Like removed')
+    })
+    .catch(error => console.error(error))
+
+})
+
+app.delete('/deleteMovie', (req, res) => {
+    db.collection('movie-names').deleteOne({"name": req.body.movieName})
+    .then(result => {
+        console.log(req.body.movieName)
+        console.log('Movie Deleted')
+        res.json('Movie Deleted')
+    })
+    .catch(error => console.error(error))
+})
+
+app.listen(process.env.PORT || PORT,()=>{
+    console.log(`The server is running on port ${PORT}`)
+})
